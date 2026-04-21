17e8a9b78f83048ce212932d2324ea90a87b558b
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-04-21 18:02:53 +0300

    original commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from './app';
+import { connectDB } from './utils/db';
+
+const start = async() => {
+  await connectDB();
+
+  app.listen(3000, () => {
+    console.log('Server is up');
+  })
+}
+
+start();
+
